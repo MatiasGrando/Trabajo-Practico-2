4e37abe316e21881fa406f401fd575df3764f2f7
MatiasGrando <matias.grando@hotmail.com>
2016-06-19 01:50:47 -0300

    Update Trabajo2.pas

--- Trabajo2.pas
+++ Trabajo2.pas
@@ -2,9 +2,11 @@ program asdasd;{Nota: Cambiar Nombre}
 
 Const
 tamNombre = 30;
+CantMeses=12;
 
 Type
 	tMes=(Ene, Feb, Mar, Abr, May, Jun, Jul, Ago, Sep, Oct, Nov, Dic);
+
 	trVentas= record
 		Fecha:string[8];{AAAAMMDD}
 		Num_Cliente:word;
@@ -13,6 +15,7 @@ Type
 		Cantidad:word;
 		importe:real;
 	end;
+
 	trSucursal= record
 		Num_Suc:word;
 		Nombre:string[tamNombre];
@@ -20,14 +23,36 @@ Type
 		Direccion:string[50];
 		Telefono:string[20];
 	end;
-taSucursal= file of trSucursal;
-taVentas= file of trVentas;
+
+	TrVentasHistorico= record
+		Anio:word;
+		Mes:1..CantMeses;
+		Num_Suc:word;
+		Importe:real;
+	end;
+
+	taVentasHistorico= file of trVentasHistorico;
+
+	taSucursal= file of trSucursal;
+
+	taVentas= file of trVentas;
 
 Var
+
 ArSuc:taSucursal;{80 Ordenado por Num_Suc (ascendente), existe un reg para cada Num_Suc}
 ArVentas:taVentas;{aprox 8000 Está ordenado en forma ascendente por Sucursal y Fecha}
+ArVentasHistorico:taVentasHistorico;
+
 
 {----------------------------------------Modulos----------------------------------------------------}
+
+procedure LeerVentasHistorico (var ar:taVentasHistorico; var rec:trVentasHistorico; var fin:boolean);
+	begin
+	fin:=eof(ar);
+	if not fin then
+	read(ar,rec);
+	end;
+
 procedure LeerSuc(var Ar:taSucursal;var Suc:trSucursal;var fin:boolean);{Lectura adelantada de sucursal}
 	begin
 	fin:=eof(Ar);
@@ -42,6 +67,7 @@ procedure LeerVentas(var Ar:taVentas;var Ven:trVentas;var fin:boolean);{Lectura 
 	read(Ar,Ven);
 	end;
 
+{---------------------PUNTO 1--------------------}
 
 procedure MostrarTabla(var ArV:taVentas; var ArS:taSucursal);{Arma la tabla}
 
@@ -119,11 +145,173 @@ begin
 	 close(ArV);
 end;
 
+{------------PUNTO 2-------------------}
+
+
+procedure TransformarVentas(var arVentas:taVentas; var arTransformarVentas:taVentasHistorico);
+
+type
+	tAuxFecha=string[4];
+	tAuxFecha2=string[2];
+
+Var
+	rVentas:trVentas;
+	rTransformarVentas:trVentasHistorico;
+	rAux:trVentasHistorico;
+        FinVentas:boolean;
+        codigo:byte;
+        AuxFecha:tAuxFecha;
+        AuxFecha2:tAuxFecha2;
+        AuxAnio:word;
+        AuxMes:byte;
+
+Begin
+	reset(arVentas);
+	rewrite(arTransformarVentas);
+	LeerVentas(arVentas,rVentas,FinVentas);
+	while not FinVentas do
+	begin
+		AuxFecha:=copy(rVentas.Fecha,1,4);
+		val(AuxFecha,AuxAnio,codigo);
+			if codigo=0 then
+				rAux.Anio:=AuxAnio
+			else writeln('Hubo un error al convertir año.');
+		AuxFecha2:=copy(rVentas.Fecha,5,2);
+		val(AuxFecha2,AuxMes,codigo);
+			if codigo=0 then
+				rAux.Mes:=AuxMes
+			else writeln('Hubo un error al convertir mes.');
+		rAux.Num_Suc:=rVentas.Sucursal;
+		rAux.Importe:=rVentas.Importe;
+		write(arTransformarVentas,rAux);
+		LeerVentas(arVentas,rVentas,FinVentas);
+	end;
+	close(arVentas);
+	close(arTransformarVentas);
+end;
+
+
+
+
+procedure Merge (var arTransformarVentas:taVentasHistorico; var arVentasHistorico:taVentasHistorico; var arHistoricoAux:taVentasHistorico);
+
+var
+
+rTransformarVentas,rVentasHistorico,rHistoricoAux: trVentasHistorico;
+finTransformarVentas, finVentasHistorico: boolean;
+
+begin
+	reset(arTransformarVentas);
+	reset(arVentasHistorico);
+	rewrite(arHistoricoAux);
+	LeerVentasHistorico(arTransformarVentas,rTransformarVentas,finTransformarVentas);
+	LeerVentasHistorico(arVentasHistorico,rVentasHistorico,finVentasHistorico);
+	while not finTransformarVentas and not finVentasHistorico do
+		begin
+		if (rTransformarVentas.Num_Suc<rVentasHistorico.Num_Suc) then
+			begin
+				write(arHistoricoAux,rTransformarVentas);
+				LeerVentasHistorico(arTransformarVentas,rTransformarVentas,finTransformarVentas);
+			end;
+		if (rTransformarVentas.Num_Suc>rVentasHistorico.Num_Suc) then
+			Begin
+				write(arHistoricoAux,rVentasHistorico);
+				LeerVentasHistorico(arVentasHistorico,rVentasHistorico,finVentasHistorico);
+			end;
+		if(rTransformarVentas.Num_Suc=rVentasHistorico.Num_Suc) then
+			Begin
+				if (rTransformarVentas.Anio<rVentasHistorico.Anio) then
+					Begin
+						write(arHistoricoAux,rTransformarVentas);
+						LeerVentasHistorico(arTransformarVentas,rTransformarVentas,finTransformarVentas);
+					end;
+				if (rTransformarVentas.Anio>rVentasHistorico.Anio) then
+					Begin
+						write(arHistoricoAux,rVentasHistorico);
+						LeerVentasHistorico(arVentasHistorico,rVentasHistorico,finVentasHistorico);
+					end;
+				if (rTransformarVentas.Anio=rVentasHistorico.Anio) then
+					Begin
+						if (rTransformarVentas.Mes<rVentasHistorico.Mes) then
+						begin
+							write(arHistoricoAux,rTransformarVentas);
+							LeerVentasHistorico(arTransformarVentas,rTransformarVentas,finTransformarVentas);
+						end;
+						if (rTransformarVentas.Mes>rVentasHistorico.Mes) then
+						Begin
+							write(arHistoricoAux,rVentasHistorico);
+							LeerVentasHistorico(arVentasHistorico,rVentasHistorico,finVentasHistorico);
+						end;
+						if (rTransformarVentas.Mes=rVentasHistorico.Mes) then
+						begin
+							write(arHistoricoAux,rTransformarVentas);
+							LeerVentasHistorico(arTransformarVentas,rTransformarVentas,finTransformarVentas);
+						end;
+					end;
+			end;
+		end;
+	while not finTransformarVentas do
+		begin
+			write(arHistoricoAux,rTransformarVentas);
+			LeerVentasHistorico(arTransformarVentas,rTransformarVentas,finTransformarVentas);
+		end;
+	while not finVentasHistorico do
+		begin
+			write(arHistoricoAux,rVentasHistorico);
+			LeerVentasHistorico(arVentasHistorico,rVentasHistorico,finVentasHistorico);
+		end;
+	close(arTransformarVentas);
+	close(arVentasHistorico);
+	close(arHistoricoAux);
+	end;
+
+
+
+procedure ActualizoHistorico(var arVentasHistorico:taVentasHistorico;var arHistoricoAux:taVentasHistorico);
+
+var
+	rHistoricoAux:trVentasHistorico;
+	finHistoricoAux:boolean;
+
+begin
+	reset(arHistoricoAux);
+	rewrite(arVentasHistorico);
+	LeerVentasHistorico(arHistoricoAux,rHistoricoAux,finHistoricoAux);
+	while not finHistoricoAux do
+	begin
+		write(arVentasHistorico,rHistoricoAux);
+		LeerVentasHistorico(arHistoricoAux,rHistoricoAux,finHistoricoAux);
+	end;
+	close(arHistoricoAux);
+	close(arVentasHistorico);
+end;
+
+
+
+
+procedure Actualizar (var ArVentasHistorico:taVentasHistorico; var ArVentas:taVentas);
+
+Var
+
+	ArTransformarVentas:taVentasHistorico;
+	ArHistoricoAux:taVentasHistorico;
+
+Begin
+	Assign(arTransformarVentas,'C:/TransformarVentas.dat');
+	Assign(arHistoricoAux,'C:/HistoricoAuxiliar.dat');
+	TransformarVentas(arVentas,arTransformarVentas);
+	Merge(arTransformarVentas,arVentasHistorico,ArHistoricoAux);
+	ActualizoHistorico(arVentasHistorico,arHistoricoAux);
+end;
+
+
 
 {-----------------------------------Programa Principal----------------------------------------------}
 begin
 	Assign(ArSuc,'C:/SucursalesArg.dat');
 	Assign(ArVentas,'C:/VentasArg2015.dat');
+	Assign(ArVentasHistorico,'C:/VentasHistoricas.dat');
 	MostrarTabla(ArVentas,ArSuc);
+	Actualizar(ArVentasHistorico,ArVentas);
 	readln();
 end.
