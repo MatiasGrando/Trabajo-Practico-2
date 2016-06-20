b387cb1016c92292ba3de5831fb07fe2ef78d874
MatiasGrando <matias.grando@hotmail.com>
2016-06-20 19:24:32 -0300

    Update Trabajo2.pas

--- Trabajo2.pas
+++ Trabajo2.pas
@@ -3,6 +3,7 @@ program asdasd;{Nota: Cambiar Nombre}
 Const
 tamNombre = 30;
 CantMeses=12;
+tamProvLoca=50;
 
 Type
 	tMes=(Ene, Feb, Mar, Abr, May, Jun, Jul, Ago, Sep, Oct, Nov, Dic);
@@ -30,19 +31,30 @@ Type
 		Num_Suc:word;
 		Importe:real;
 	end;
+	
+	TrClientes= record
+		Num_Cli:Longint;
+		Nombre:String[30];
+		Provincia:String[50];
+		Localidad:String[50];
+		Direccion:String[50];
+	end;
 
 	taVentasHistorico= file of trVentasHistorico;
 
 	taSucursal= file of trSucursal;
 
 	taVentas= file of trVentas;
+	
+	TaClientes = file of trClientes;
 
 Var
 
 ArSuc:taSucursal;{80 Ordenado por Num_Suc (ascendente), existe un reg para cada Num_Suc}
 ArVentas:taVentas;{aprox 8000 Está ordenado en forma ascendente por Sucursal y Fecha}
 ArVentasHistorico:taVentasHistorico;
-
+ArCli:taClientes;
+ArTotCli:Text;
 
 
 
@@ -68,7 +80,13 @@ procedure LeerVentas(var Ar:taVentas;var Ven:trVentas;var fin:boolean);{Lectura 
 	if not fin then
 	read(Ar,Ven);
 	end;
-
+	
+procedure LeerClientes(var Ar:taClientes;var Cli:trClientes;var fin:boolean);{Lectura adelantada de clientes}
+	begin
+	fin:=eof(Ar);
+	if not fin then
+	read(Ar,Cli);
+	end;
 {---------------------PUNTO 1--------------------}
 
 procedure MostrarTabla(var ArV:taVentas; var ArS:taSucursal);{Arma la tabla}
@@ -345,14 +363,106 @@ Procedure ActualizarArchSucMun;
 		close(archSucMun);
 		close(archSucArg);
 
+{------------PUNTO 3-------------------}
+
+procedure GenerarTotCli (var ArCli:taClientes; var ArTotCli:text);
+
+Type
+Cad50=String[50];
+
+Var
+Clientes:TrClientes;
+fin:boolean;
+primero:boolean;
+ContProv:longint;
+ContLoca:longint;
+ContClien:longint;
+ProvAnt:Cad50;
+LocaAnt:Cad50;
+
+	Procedure GuardaProvincia(var ArTotCli:text; var ProvAnt:Cad50; var ContProv:longint; Clientes:TrClientes);
+	Var
+	i:byte;
+	
+	Begin
+	Write(ArTotCli, ProvAnt);
+	for i:=1 to (tamProvLoca - length(ProvAnt)) do
+		write(' ');
+	Writeln(ArTotCli, ContProv);
+	ContProv:=0;
+	ProvAnt:=Clientes.Provincia;
+	End;
+
+	Procedure GuardaLocalidad(var ArTotCli:text; var LocaAnt:Cad50; var ContLoca:longint; Clientes:TrClientes);
+	Var
+	i:byte;
+
+	Begin
+	Write(ArTotCli, LocaAnt);
+	for i:=1 to (tamProvLoca - length(LocaAnt)) do
+		write(' ');
+	Writeln(ArTotCli, ContLoca);
+	ContLoca:=0;
+	LocaAnt:=Clientes.Localidad;
+	End;
+
+	Procedure GuardaClientes(var ArTotCli:text; var ContClien:longint);
+	Begin
+	Write(ArTotCli, 'Clientes totales:');
+	Writeln(ArTotCli, ContClien);
+	ContClien:=0;
+	End;
+	
+	Procedure CargaTotCli(var ArTotCli:Text; var Clientes:TrClientes; var ContProv, ContLoca, ContClien:longint; var primero:boolean; var ProvAnt, LocaAnt:Cad50);
+	Begin
+	If Primero then
+		Begin
+		Primero:=False;
+		ProvAnt:=Clientes.Provincia;
+		LocaAnt:=Clientes.Localidad;
+		End;
+	If ProvAnt<>Clientes.Provincia then
+		Begin 
+		GuardaLocalidad(ArTotCli, LocaAnt, ContLoca, Clientes);
+		GuardaProvincia(ArTotCli, ProvAnt, ContProv, Clientes);
+		End
+	Else
+	If LocaAnt<>Clientes.Localidad then
+		GuardaLocalidad(ArTotCli, LocaAnt, ContLoca, Clientes);
+	Inc(ContProv);
+	Inc(ContLoca);
+	Inc(ContClien);
+	End;
+
+Begin
+Reset(ArCli);
+Rewrite(ArTotCli);
+LeerClientes(ArCli, Clientes, fin);
+Primero:=True;
+While not fin do
+begin
+	CargaTotCli(ArTotCli, Clientes, ContProv, ContLoca, ContClien, primero, ProvAnt, LocaAnt);
+	LeerClientes(ArCli, Clientes, fin);
+end;
+GuardaLocalidad(ArTotCli, LocaAnt, ContLoca, Clientes);
+GuardaProvincia(ArTotCli, ProvAnt, ContProv, Clientes);
+GuardaClientes(ArTotCli, ContClien);
+close(ArCli);
+close(ArTotCli);
+End;
+
+
 {-----------------------------------Programa Principal----------------------------------------------}
 begin
 	Assign(ArSuc,'C:\SucursalesArg.dat');
 	Assign(ArVentas,'C:\VentasArg2015.dat');
 	Assign(ArVentasHistorico,'C:\VentasHistoricas.dat');
 	assing(archSucMun,'c:\SucMun.dat');
+	Assign(ArCli,'C:/Clientes.dat');
+	Assign(ArTotCli,'C:/TotCli.txt');	
 	MostrarTabla(ArVentas,ArSuc);
 	Actualizar(ArVentasHistorico,ArVentas);
 	ActualizarArchSucMun;
+	GenerarTotCli(ArCli, ArTotCli);
 	readln();
 end.
