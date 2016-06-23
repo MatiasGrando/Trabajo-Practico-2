6a57ffb992d1c3027ced08936b3260da58714e89
MatiasGrando <matias.grando@hotmail.com>
2016-06-23 18:35:27 -0300

    Update SucMundo.pas

--- SucMundo.pas
+++ SucMundo.pas
@@ -1,5 +1,7 @@
 program CargarSucArg;
+	
 	type
+	
 	trSucursalesArg= record
 		Num_Sucursal:word;
 		Nombre:string[30];
@@ -8,25 +10,28 @@ program CargarSucArg;
 		Telefono:string[20];
                 end;
         tasucursalesarg  = file of trsucursalesarg;
+	
 	var
+	
 	ArchSucMundo:taSucursalesArg;
 	RegSucMundo:trSucursalesArg;
-	{variables dentro del registro (auxiliares)}
 		aNum_Sucursal:word;
 		aNombre:string[30];
 		aPais:string[50];
 		aDireccion:string[50];
 		aTelefono:string[20];
 		opcion:byte;
+	
 	begin
-	assign(ArchSucMundo,'C:\tp\sucmundo.dat');
+	
+	assign(ArchSucMundo,'C:\TP\sucmundo.dat');
 	rewrite(archSucMundo);
 	writeln('Ingrese 1 para agregar nuevos registros, 0 para salir');
 	readln(opcion);
 	while (opcion <> 0) do
 	begin
 		writeln('ingrese num de suc');
-                readln(anum_sucursal);
+        readln(anum_sucursal);
 		writeln('Ingrese Nombre');
 		readln(aNombre);
 		writeln('Ingrese Pais');
